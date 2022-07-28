112c737a68fac50c854036e9c6a363b92b172114
Samuel Taylor-El Baz <staylor-elbaz@trustredline.co.uk>
2022-07-28 16:28:09 +0100

    Created register functionality

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
@@ -24,6 +25,37 @@ namespace WishList.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> RegisterAsync (RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+            var result = await _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = registerViewModel.Email,
+                Email = registerViewModel.Email,
+                PasswordHash = registerViewModel.Password
+            });
+
+            if (!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", error.Description);
+                }
+                return View(registerViewModel);
+            }
+
+            return RedirectToAction("HomeController.Index");
         }
     }
 }
